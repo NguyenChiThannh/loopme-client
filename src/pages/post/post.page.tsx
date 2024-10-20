82c70031b27a0788885ce1c3052f6e67a5d44f96
Van Duy <vanduy352003@gmail.com>
2024-10-20 18:04:32 +0700

    separate to post card component

--- src/pages/post/post.page.tsx
+++ src/pages/post/post.page.tsx
@@ -2,16 +2,11 @@ import { useRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
 
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-
-import HoverUsername from "@/features/post/components/hover-username";
-import PostAction from "@/features/post/components/post-action";
+import PostCard from "@/features/post/components/post-card";
 import PostComment from "@/features/post/components/post-comment";
 import PostCommentForm from "@/features/post/components/post-comment-form";
-import PostImage from "@/features/post/components/post-image";
 import PostReply from "@/features/post/components/post-reply";
 import PostReplyForm from "@/features/post/components/post-reply-form";
-import PostUpvote from "@/features/post/components/post-upvote";
 
 type Comment = {
     id: number;
@@ -89,22 +84,12 @@ export default function PostPage() {
     ]);
 
     const [replyingTo, setReplyingTo] = useState<number | null>(null);
-    const [isImageModalOpen, setIsImageModalOpen] = useState(false);
     const commentSectionRef = useRef<HTMLDivElement>(null);
-
-    const handleUpvote = () =>
-        setPost((prev) => ({ ...prev, upvotes: prev.upvotes + 1 }));
-    const handleDownvote = () =>
-        setPost((prev) => ({ ...prev, upvotes: prev.upvotes - 1 }));
 
     const handleReplyClick = (commentId: number) => {
         setReplyingTo((prevState) =>
             prevState === commentId ? null : commentId,
         );
-    };
-
-    const scrollToComments = () => {
-        commentSectionRef.current?.scrollIntoView({ behavior: "smooth" });
     };
 
     const renderComments = (
@@ -153,40 +138,7 @@ export default function PostPage() {
 
     return (
         <div className="mx-auto max-w-2xl p-4">
-            <Card className="mb-6">
-                <CardHeader className="flex flex-row items-start gap-4 py-3">
-                    <PostUpvote
-                        post={post}
-                        downvote={handleDownvote}
-                        upvote={handleUpvote}
-                    />
-                    <div className="flex-grow">
-                        <h1 className="text-xl font-bold">{post.title}</h1>
-                        Posted by{" "}
-                        <HoverUsername
-                            name={post.author}
-                            avatarSrc={""}
-                            karma={0}
-                            joinDate={""}
-                            cakeDay={""}
-                            description={""}
-                        />{" "}
-                        {post.postedAt}
-                    </div>
-                </CardHeader>
-                <CardContent className="py-3">
-                    <p className="mb-4 text-sm">{post.content}</p>
-                    {post.imageUrl && (
-                        <PostImage
-                            post={post}
-                            isImageModalOpen={isImageModalOpen}
-                            setIsImageModalOpen={setIsImageModalOpen}
-                        />
-                    )}
-                </CardContent>
-                <PostAction post={post} postCommentAction={scrollToComments} />
-            </Card>
-
+            <PostCard commentSectionRef={commentSectionRef} />
             <PostCommentForm />
             <div ref={commentSectionRef}>{renderComments(comments)}</div>
         </div>

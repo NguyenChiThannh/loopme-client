030478454df30c3b7da52d6c76c40174f0941999
An <anacelol1234@gmail.com>
2024-10-31 10:17:53 +0700

    adding sidebar from shadcn

--- src/layouts/root.layout.tsx
+++ src/layouts/root.layout.tsx
@@ -4,14 +4,14 @@ import { Navbar } from "@/components/navbar";
 
 export default function RootLayout() {
     return (
-        <div className="flex min-h-screen flex-col">
+        <div className="flex flex-col min-h-screen">
             <header className="sticky top-0 z-50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
                 <Navbar />
             </header>
             <main className="flex-grow">
                 <Outlet />
             </main>
-            <footer className="border-t bg-background py-4 text-center text-sm text-muted-foreground">
+            <footer className="py-4 text-sm text-center border-t bg-background text-muted-foreground">
                 This is Copyright
             </footer>
         </div>

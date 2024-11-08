fa904aa6de9cac7827d6155237cca5d1b6472e46
Van Duy <vanduy352003@gmail.com>
2024-11-08 08:22:59 +0700

    refactor

--- src/features/post/apis/service.ts
+++ src/features/post/apis/service.ts
@@ -1,7 +1,16 @@
 import axiosRequest from "@/configs/request";
-import { PostEndpoints } from "./config";
 import { AxiosMethod } from "@/configs/axios";
 import { IPost, PaginatedResponse } from "@/configs/type";
+
+export const PostEndpoints = {
+    getPosts: () => "/posts",
+    create: () => "/posts",
+    upvote: () => "/posts/:postId/upvote",
+    downvote: () => "/posts/:postId/downvote",
+    removevote: () => "/posts/:postId/removevote",
+    getPostsByGroupId: (groupId: string) => `/posts/group/${groupId}`,
+};
+
 
 export default class PostService {
     public static getPosts(page: number, size: number, sort: string): Promise<PaginatedResponse<IPost[]>> {

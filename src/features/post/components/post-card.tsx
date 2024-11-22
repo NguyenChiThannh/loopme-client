56dbebf153053181e3b2499e32f7b396cb29aded
Nguyễn Chí Thanh <92113679+NguyenChiThannh@users.noreply.github.com>
2024-11-22 19:57:16 +0700

    fixx

--- src/features/post/components/post-card.tsx
+++ src/features/post/components/post-card.tsx
@@ -70,6 +70,11 @@ export default function PostCard({ commentSectionRef, post }: PostCardProps) {
                                 </Link>
                                 <span className="text-sm text-muted-foreground">•</span>
                                 <span className="text-sm text-muted-foreground">Post by</span>
+                                <HoverUsername
+                                    _id={post.user._id}
+                                    displayName={post.user.displayName}
+                                    avatar={post.user.avatar || ""}
+                                />
                             </>
                         ) : (
                             <div className="flex items-center space-x-1 text-sm text-muted-foreground">

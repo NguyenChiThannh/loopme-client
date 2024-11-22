bca80831b9bd4f9d057f88fb5a9f9310f23c1690
An <anacelol1234@gmail.com>
2024-11-22 21:11:52 +0700

    remove pending invitation

--- src/features/group/components/group-card.tsx
+++ src/features/group/components/group-card.tsx
@@ -24,6 +24,8 @@ export function GroupCard({ group }: GroupCardProps) {
     const { user, isLoading } = useUser();
     const { mutate: handleJoinGroup, isPending } =
         groupApi.mutation.useSendJoinRequest();
+    const groupMutate = groupApi.mutation.useRemovePendingJoinInvitation();
+
     const navigate = useNavigate();
 
     if (isLoading || isPending) {
@@ -37,6 +39,7 @@ export function GroupCard({ group }: GroupCardProps) {
     const canJoinGroup =
         !isOwner &&
         (group.status === "not_joined" || group.status !== "pending");
+
     console.log('Group" ', group);
     return (
         <Card className="w-full max-w-md">
@@ -62,7 +65,7 @@ export function GroupCard({ group }: GroupCardProps) {
                 </div>
             </CardContent>
             <CardFooter>
-                {!canJoinGroup ? (
+                {/* {!canJoinGroup ? (
                     <Button className="w-full" disabled>
                         {(() => {
                             switch (group.status) {
@@ -83,7 +86,51 @@ export function GroupCard({ group }: GroupCardProps) {
                     >
                         Join Group
                     </Button>
-                )}
+                )} */}
+                {(() => {
+                    switch (group.status) {
+                        case "pending":
+                            return (
+                                <Button
+                                    className="w-full"
+                                    onClick={() => {
+                                        groupMutate.mutate({
+                                            groupId: group._id,
+                                            userId: user._id,
+                                        });
+                                    }}
+                                >
+                                    Remove
+                                </Button>
+                            );
+                        case "joined":
+                            return (
+                                <Button className="w-full" disabled={true}>
+                                    Joined
+                                </Button>
+                            );
+                        case "not_joined":
+                            return !isOwner ? (
+                                <Button
+                                    className="w-full"
+                                    onClick={() => handleJoinGroup(group._id)}
+                                    disabled={isPending}
+                                >
+                                    Join Group
+                                </Button>
+                            ) : (
+                                <Button className="w-full" disabled={true}>
+                                    Joined
+                                </Button>
+                            );
+                        default:
+                            return (
+                                <Button className="w-full" disabled={true}>
+                                    Joined
+                                </Button>
+                            );
+                    }
+                })()}
             </CardFooter>
         </Card>
     );
